1d4f047c9b850298f7462f7fb89ccca89cf82df4
Njade <njade412@gmail.com>
2020-07-27 00:27:37 +0900

    add: oauth2 properties (#3)

---
+++ src/main/kotlin/com/njade/kotlinlogin/config/property/OAuth2Property.kt
@@ -0,0 +1,16 @@
+package com.njade.kotlinlogin.config.property
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ConfigurationProperties(prefix = "oauth2")
+class OAuth2Property {
+
+    var clients: Map<String, Oauth2ClientProperty> = HashMap()
+
+    class Oauth2ClientProperty {
+        var clientId: String? = null
+        var clientSecret: String? = null
+    }
+}
