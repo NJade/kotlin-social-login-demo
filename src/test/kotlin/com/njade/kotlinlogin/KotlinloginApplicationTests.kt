b41974790a369ac7527347ca882fbd0d8f8317ed
Njade <njade412@gmail.com>
2020-07-26 23:19:44 +0900

    init: project

---
+++ src/test/kotlin/com/njade/kotlinlogin/KotlinloginApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.njade.kotlinlogin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinloginApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
